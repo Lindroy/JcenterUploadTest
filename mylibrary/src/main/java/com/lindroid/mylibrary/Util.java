f838eba9551dbd7fd876e150e7cc3149d4d8aab1
Lindroy <linyulong2014@163.com>
2019-07-01 11:03:18 +0800

    发布成功。

---
+++ mylibrary/src/main/java/com/lindroid/mylibrary/Util.java
@@ -0,0 +1,13 @@
+package com.lindroid.mylibrary;
+
+/**
+ * @author Lin
+ * @date 2019/7/1
+ * @function
+ * @Description
+ */
+public class Util {
+    public static String getLibDesc(){
+        return "This is my lib.";
+    }
+}
